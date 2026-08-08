e5361edce0024166433ef3389b9b85f18c710d2e
davontemerritt93 <davonte.merritt93@gmail.com>
2026-08-08 05:21:03 -0400

    Modernize LOTB core manifest

--- resources/[lotb]/lotb_core/fxmanifest.lua
+++ resources/[lotb]/lotb_core/fxmanifest.lua
@@ -4,22 +4,20 @@ game 'gta5'
 name 'lotb_core'
 author 'Land of the Bloody RP'
 description 'Shared security, audit and identity helpers'
-version '0.2.0'
-
-lua54 'yes'
+version '0.3.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
-    'config/shared.lua',
+    'config/shared.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server/main.lua',
+    'server/main.lua'
 }
 
 dependencies {
     'ox_lib',
     'oxmysql',
-    'qbx_core',
+    'qbx_core'
 }
