c48a0f6959ad8d12fbebf6b0f68015201fc453ee
davontemerritt93 <davonte.merritt93@gmail.com>
2026-08-08 05:13:06 -0400

    Configure City Memory districts

---
+++ resources/[lotb]/lotb_citymemory/config/shared.lua
@@ -0,0 +1,27 @@
+return {
+    memoryWindowDays = 45,
+    minScore = -100,
+    maxScore = 100,
+    districts = {
+        ['DOWNT'] = 'downtown',
+        ['PBOX'] = 'downtown',
+        ['TEXTI'] = 'downtown',
+        ['STRAW'] = 'south_ls',
+        ['DAVIS'] = 'south_ls',
+        ['RANCHO'] = 'south_ls',
+        ['CHAMH'] = 'south_ls',
+        ['MIRR'] = 'east_ls',
+        ['LMESA'] = 'east_ls',
+        ['CYPRE'] = 'east_ls',
+        ['VESP'] = 'vespucci',
+        ['BEACH'] = 'vespucci',
+        ['VCANA'] = 'vespucci',
+        ['WVINE'] = 'vinewood',
+        ['VINE'] = 'vinewood',
+        ['RICHM'] = 'vinewood',
+        ['AIRP'] = 'airport',
+        ['ZANCUDO'] = 'county',
+        ['SANDY'] = 'county',
+        ['PALETO'] = 'county'
+    }
+}
