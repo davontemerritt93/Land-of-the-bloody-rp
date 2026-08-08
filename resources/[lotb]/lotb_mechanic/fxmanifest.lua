155004272937022d2a99cf3f3610490edbd8dda2
davontemerritt93 <davonte.merritt93@gmail.com>
2026-08-08 05:30:38 -0400

    Add LOTB mechanic manifest

---
+++ resources/[lotb]/lotb_mechanic/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'lotb_mechanic'
+author 'Land of the Bloody RP'
+description 'Persistent vehicle work orders and service history'
+version '0.4.0'
+
+shared_script '@ox_lib/init.lua'
+server_scripts {
+  '@oxmysql/lib/MySQL.lua',
+  'server/main.lua'
+}
+client_script 'client/main.lua'
+
+dependencies {
+  'lotb_core',
+  'ox_lib',
+  'oxmysql',
+  'qbx_core',
+  'qbx_vehicles'
+}
