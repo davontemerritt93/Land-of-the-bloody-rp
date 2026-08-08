924414b7c3ad45dc96e6b703c1c6f3ade4b2834a
davontemerritt93 <davonte.merritt93@gmail.com>
2026-08-08 05:45:38 -0400

    Configure LOTB insurance plans

---
+++ resources/[lotb]/lotb_insurance/config/shared.lua
@@ -0,0 +1,9 @@
+return {
+  plans = {
+    basic = { label = 'Basic', coverage = 50000, deductible = 2500, premium = 750 },
+    standard = { label = 'Standard', coverage = 150000, deductible = 5000, premium = 1800 },
+    premium = { label = 'Premium', coverage = 500000, deductible = 10000, premium = 4500 }
+  },
+  termDays = 30,
+  maxEvidenceRefs = 5
+}
