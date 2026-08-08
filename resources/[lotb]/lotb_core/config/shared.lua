53a4d10964165bbc0497267b0d170788960d465b
davontemerritt93 <davonte.merritt93@gmail.com>
2026-08-08 04:56:51 -0400

    Add LOTB core config

---
+++ resources/[lotb]/lotb_core/config/shared.lua
@@ -0,0 +1,12 @@
+return {
+    serverName = 'Land of the Bloody RP',
+    tagline = 'The city remembers.',
+    maxText = 255,
+    permissions = {
+        admin = 'lotb.admin',
+        justice = 'lotb.justice.manage',
+        business = 'lotb.business.manage',
+        crew = 'lotb.crew.manage',
+        medical = 'lotb.medical.manage'
+    }
+}
